e513a515be8d5cff23422721529b051ace1bb8d4
bokaei <m.hadibokaei@gmail.com>
2019-07-06 12:32:48 +0430

    updates

--- mtl_config.py
+++ mtl_config.py
@@ -1,55 +1,31 @@
 class MTLConfig():
 
-    def __init__(self, directory_data_param, file_conll_task1_directory_param, file_conll_task2_directory_param, file_conll_task3_directory_param, file_conll_task4_directory_param, task_param):
+    def __init__(self, base_directory_path):
 
-        self.task = task_param
-        self.gan = False
-        self.directory_data = directory_data_param
+        self.base_directory_path = base_directory_path
+        self.base_directory_data = self.base_directory_path + "/data/"
+        self.base_directory_model = self.base_directory_path + "/model/"
 
-        #task1:
-        self.file_conll_task1_directory = file_conll_task1_directory_param
+        #NER_Bijankhan:
+        self.file_conll_task1_directory = self.base_directory_data + "ner_bijankhan/"
         self.file_conll_task1_train_data = self.file_conll_task1_directory + "train.data"
         self.file_conll_task1_validation_data = self.file_conll_task1_directory + "validation.data"
-        self.file_conll_task1_test_data = self.file_conll_task1_directory + "test.data"
         self.file_seq_task1_train_data = self.file_conll_task1_train_data + ".seq"
         self.file_seq_task1_validation_data = self.file_conll_task1_validation_data + ".seq"
-        self.file_seq_task1_test_data = self.file_conll_task1_test_data + ".seq"
 
-        #task2:
-        self.file_conll_task2_directory = file_conll_task2_directory_param
+        #NER_ArmanPerso:
+        self.file_conll_task2_directory = self.base_directory_data + "ner_armanperso/"
         self.file_conll_task2_train_data = self.file_conll_task2_directory + "train.data"
-        self.file_conll_task2_validation_data = self.file_conll_task2_directory + "validation.data"
-        self.file_conll_task2_test_data = self.file_conll_task2_directory + "test.data"
         self.file_seq_task2_train_data = self.file_conll_task2_train_data + ".seq"
-        self.file_seq_task2_validation_data = self.file_conll_task2_validation_data + ".seq"
-        self.file_seq_task2_test_data = self.file_conll_task2_test_data + ".seq"
 
-        #task3:
-        self.file_conll_task3_directory = file_conll_task3_directory_param
-        self.file_conll_task3_train_data = self.file_conll_task3_directory + "train.data"
-        self.file_conll_task3_validation_data = self.file_conll_task3_directory + "validation.data"
-        self.file_conll_task3_test_data = self.file_conll_task3_directory + "test.data"
-        self.file_seq_task3_train_data = self.file_conll_task3_train_data + ".seq"
-        self.file_seq_task3_validation_data = self.file_conll_task3_validation_data + ".seq"
-        self.file_seq_task3_test_data = self.file_conll_task3_test_data + ".seq"
-
-        #task4:
-        self.file_conll_task4_directory = file_conll_task4_directory_param
-        self.file_conll_task4_train_data = self.file_conll_task4_directory + "train.data"
-        self.file_conll_task4_validation_data = self.file_conll_task4_directory + "validation.data"
-        self.file_conll_task4_test_data = self.file_conll_task4_directory + "test.data"
-        self.file_seq_task4_train_data = self.file_conll_task4_train_data + ".seq"
-        self.file_seq_task4_validation_data = self.file_conll_task4_validation_data + ".seq"
-        self.file_seq_task4_test_data = self.file_conll_task4_test_data + ".seq"
-
-        self.file_full_word_embedding = self.directory_data + "wiki_news_fasttext_sg_d300_w10.vec"
-        self.file_trimmed_word_embedding = self.directory_data + "word_embedding.trimmed"
-        self.file_word_vocab = self.directory_data + "vocab.words"
-        self.file_char_vocab = self.directory_data + "vocab.chars"
-        self.file_tag_vocab = self.directory_data + "vocab.tags"
+        self.file_full_word_embedding = self.base_directory_path + "wiki_news_fasttext_sg_d300_w10.vec"
+        self.file_trimmed_word_embedding = self.base_directory_model + "word_embedding.trimmed"
+        self.file_word_vocab = self.base_directory_model + "vocab.words"
+        self.file_char_vocab = self.base_directory_model + "vocab.chars"
+        self.file_tag_vocab = self.base_directory_model + "vocab.tags"
 
         self.dir_tensoboard_log = "log/"
-        self.dir_checkpoints = "chkpnts/"
+        self.dir_checkpoints = self.base_directory_model
 
         self.word_embedding_dimension        = 300
         self.char_embedding_dimension        = 100
--- script_mtl_train.py
+++ script_mtl_train.py
@@ -1,146 +1,49 @@
-from model.mtl2_model import MTL2Model
-from model.mtl3_model import MTL3Model
-from model.mtl4_model import MTL4Model
-from model.mtl2_gan_model import MTL2GANModel
-from model.mtl3_gan_model import MTL3GANModel
-from model.mtl2_char_cnn_word_bilstm_model import MTL2CharCNNWordBilstmModel
-from model.mtl3_char_cnn_word_bilstm_model import MTL3CharCNNWordBilstmModel
-from model.mtl4_char_cnn_word_bilstm_model import MTL4CharCNNWordBilstmModel
+from mtl2_char_cnn_word_bilstm_model import MTL2CharCNNWordBilstmModel
+from utility import *
+from mtl_config import MTLConfig
 
+logger = setup_custom_logger(__name__)
 
-from common.utility import *
-from common.mtl_config import MTLConfig
+cfg = MTLConfig("files/")
 
-numtask = sys.argv[1]
+logger.info("bulding vocabulray")
+[word_vocab, tag_vocab, char_vocab, max_char] = build_vocab([cfg.file_conll_task1_train_data, cfg.file_conll_task1_validation_data])
 
-if numtask == "mtl2":
-    task1 = sys.argv[2]
-    task2 = sys.argv[3]
-    task3 = ""
-    task4 = ""
-elif numtask == "mtl3":
-    task1 = sys.argv[2]
-    task2 = sys.argv[3]
-    task3 = sys.argv[4]
-    task4 = ""
-elif numtask == "mtl4":
-    task1 = sys.argv[2]
-    task2 = sys.argv[3]
-    task3 = sys.argv[4]
-    task4 = sys.argv[5]
-elif numtask == "mtl2charcnnwordbilstm":
-    task1 = sys.argv[2]
-    task2 = sys.argv[3]
-    task3 = ""
-    task4 = ""
-elif numtask == "mtl3charcnnwordbilstm":
-    task1 = sys.argv[2]
-    task2 = sys.argv[3]
-    task3 = sys.argv[4]
-    task4 = ""
-elif numtask == "mtl4charcnnwordbilstm":
-    task1 = sys.argv[2]
-    task2 = sys.argv[3]
-    task3 = sys.argv[4]
-    task4 = sys.argv[5]
+logger.info("dumping created vocabulray")
+dump_vocab(word_vocab, cfg.file_word_vocab)
+dump_vocab(tag_vocab, cfg.file_tag_vocab)
+dump_vocab(char_vocab, cfg.file_char_vocab)
 
-cfg = MTLConfig(directory_data_param="/home/itrc/hadi/workspace/persian_ner/files/mtl/data/"
-                             , file_conll_task1_directory_param="/home/itrc/hadi/workspace/persian_ner/files/mtl/data/" + task1 + "/"
-                             , file_conll_task2_directory_param="/home/itrc/hadi/workspace/persian_ner/files/mtl/data/" + task2 + "/"
-                             , file_conll_task3_directory_param="/home/itrc/hadi/workspace/persian_ner/files/mtl/data/" + task3 + "/"
-                             , file_conll_task4_directory_param="/home/itrc/hadi/workspace/persian_ner/files/mtl/data/" + task4 + "/"
-                             , task_param=numtask)
-
-[vocab_id2tag, vocab_tag2id] = load_vocab(cfg.file_tag_vocab)
+logger.info("loading created vocabulray again")
 [vocab_id2word, vocab_word2id] = load_vocab(cfg.file_word_vocab)
+[vocab_id2tag, vocab_tag2id] = load_vocab(cfg.file_tag_vocab)
 [vocab_id2char, vocab_char2id] = load_vocab(cfg.file_char_vocab)
 
+logger.info("trimming word embedding")
+dump_trimmed_word_embeddings(vocab_word2id, cfg.file_full_word_embedding, cfg.file_trimmed_word_embedding,cfg.word_embedding_dimension)
 twe = load_trimmed_word_embeddings(cfg.file_trimmed_word_embedding)
+
+logger.info("converting to conll format")
+convert_conll_to_numpy_array(cfg.file_conll_task1_train_data, vocab_word2id, vocab_tag2id, vocab_char2id,
+                             cfg.file_seq_task1_train_data, cfg.max_char)
+convert_conll_to_numpy_array(cfg.file_conll_task1_validation_data, vocab_word2id, vocab_tag2id, vocab_char2id,
+                             cfg.file_seq_task1_validation_data, cfg.max_char)
+convert_conll_to_numpy_array(cfg.file_conll_task2_train_data, vocab_word2id, vocab_tag2id, vocab_char2id,
+                             cfg.file_seq_task2_train_data, cfg.max_char)
+
+
 [vocab_size, dim] = np.shape(twe)
 tag_size = max(vocab_id2tag, key=int) + 1
 char_size = max(vocab_id2char, key=int) + 1
 
+logger.info("loading data again.")
 [validation_words, validation_tags, validation_chars] = load_sequence_data(cfg.file_seq_task1_validation_data)
-
 [task1_train_words, task1_train_tags, task1_train_chars] = load_sequence_data(cfg.file_seq_task1_train_data)
-
 [task2_train_words, task2_train_tags, task2_train_chars] = load_sequence_data(cfg.file_seq_task2_train_data)
 
-if cfg.task == "mtl3" or cfg.task == "mtl3charcnnwordbilstm":
-    [task3_train_words, task3_train_tags, task3_train_chars] = load_sequence_data(cfg.file_seq_task3_train_data)
 
-if cfg.task == "mtl4" or cfg.task == "mtl4charcnnwordbilstm":
-    [task3_train_words, task3_train_tags, task3_train_chars] = load_sequence_data(cfg.file_seq_task3_train_data)
-    [task4_train_words, task4_train_tags, task4_train_chars] = load_sequence_data(cfg.file_seq_task4_train_data)
-
-
-if cfg.task == "mtl2":
-    if cfg.gan:
-        mod = MTL2GANModel(vocab_size, dim, tag_size, tag_size)
-        mod.build_graph()
-        mod.train_graph(task1_train_word_seq=task1_train_words, task1_train_tag_seq=task1_train_tags
-                        , task2_train_word_seq=task2_train_words, task2_train_tag_seq=task2_train_tags
-                        , word_embedding=twe)
-    else:
-        mod = MTL2CharCNNWordBilstmModel(vocab_size, dim, tag_size, tag_size, cfg)
-        mod.build_graph()
-        mod.train_graph(task1_train_word_seq=task1_train_words, task1_train_tag_seq=task1_train_tags
-                        , task2_train_word_seq=task2_train_words, task2_train_tag_seq=task2_train_tags
-                        , word_embedding=twe)
-
-if cfg.task == "mtl3":
-    if cfg.gan:
-        mod = MTL3GANModel(vocab_size, dim, tag_size, tag_size, tag_size)
-        mod.build_graph()
-        mod.train_graph(task1_train_word_seq=task1_train_words, task1_train_tag_seq=task1_train_tags
-                        , task2_train_word_seq=task2_train_words, task2_train_tag_seq=task2_train_tags
-                        , task3_train_word_seq=task3_train_words, task3_train_tag_seq=task3_train_tags
-                        , word_embedding=twe)
-    else:
-        mod = MTL3Model(vocab_size, dim, tag_size, tag_size, tag_size, cfg)
-        mod.build_graph()
-        mod.train_graph(task1_train_word_seq=task1_train_words, task1_train_tag_seq=task1_train_tags
-                        , task2_train_word_seq=task2_train_words, task2_train_tag_seq=task2_train_tags
-                        , task3_train_word_seq=task3_train_words, task3_train_tag_seq=task3_train_tags
-                        , word_embedding=twe)
-
-if cfg.task == "mtl4":
-    if cfg.gan:
-        print("nothing to do")
-    else:
-        mod = MTL4Model(vocab_size, dim, tag_size, tag_size, tag_size, tag_size, cfg)
-        mod.build_graph()
-        mod.train_graph(task1_train_word_seq=task1_train_words, task1_train_tag_seq=task1_train_tags
-                        , task2_train_word_seq=task2_train_words, task2_train_tag_seq=task2_train_tags
-                        , task3_train_word_seq=task3_train_words, task3_train_tag_seq=task3_train_tags
-                        , task4_train_word_seq=task4_train_words, task4_train_tag_seq=task4_train_tags
-                        , word_embedding=twe)
-
-if cfg.task == "mtl2charcnnwordbilstm":
-    mod = MTL2CharCNNWordBilstmModel(vocab_size, dim, tag_size, tag_size, cfg, char_size)
-    mod.build_graph()
-    mod.train_graph(task1_train_word_seq=task1_train_words, task1_train_tag_seq=task1_train_tags, task1_train_char_seq=task1_train_chars
-                    , task2_train_word_seq=task2_train_words, task2_train_tag_seq=task2_train_tags, task2_train_char_seq=task2_train_chars
-                    , word_embedding=twe)
-
-if cfg.task == "mtl3charcnnwordbilstm":
-    mod = MTL3CharCNNWordBilstmModel(vocab_size, dim, tag_size, tag_size, tag_size, cfg)
-    mod.build_graph()
-    mod.train_graph(task1_train_word_seq=task1_train_words, task1_train_tag_seq=task1_train_tags, task1_train_char_seq=task1_train_chars
-                    , task2_train_word_seq=task2_train_words, task2_train_tag_seq=task2_train_tags, task2_train_char_seq=task2_train_chars
-                    , task3_train_word_seq=task3_train_words, task3_train_tag_seq=task3_train_tags, task3_train_char_seq=task3_train_chars
-                    , word_embedding=twe)
-
-if cfg.task == "mtl4charcnnwordbilstm":
-    mod = MTL4CharCNNWordBilstmModel(vocab_size, dim, tag_size, tag_size, tag_size, tag_size, cfg)
-    mod.build_graph()
-    mod.train_graph(task1_train_word_seq=task1_train_words, task1_train_tag_seq=task1_train_tags, task1_train_char_seq=task1_train_chars
-                    , task2_train_word_seq=task2_train_words, task2_train_tag_seq=task2_train_tags, task2_train_char_seq=task2_train_chars
-                    , task3_train_word_seq=task3_train_words, task3_train_tag_seq=task3_train_tags, task3_train_char_seq=task3_train_chars
-                    , task4_train_word_seq=task4_train_words, task4_train_tag_seq=task4_train_tags, task4_train_char_seq=task4_train_chars
-                    , word_embedding=twe)
-
-
-
-
-
+mod = MTL2CharCNNWordBilstmModel(vocab_size, dim, tag_size, tag_size, cfg, char_size)
+mod.build_graph()
+mod.train_graph(task1_train_word_seq=task1_train_words, task1_train_tag_seq=task1_train_tags, task1_train_char_seq=task1_train_chars
+                , task2_train_word_seq=task2_train_words, task2_train_tag_seq=task2_train_tags, task2_train_char_seq=task2_train_chars
+                , word_embedding=twe)
