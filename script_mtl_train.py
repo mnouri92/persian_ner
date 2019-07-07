ecbb6665f2a95933d2cf9f4baa95c979885baed4
bokaei <m.hadibokaei@gmail.com>
2019-07-08 01:13:20 +0430

    Update

--- script_mtl_train.py
+++ script_mtl_train.py
@@ -6,22 +6,28 @@ logger = setup_custom_logger(__name__)
 
 cfg = MTLConfig("files/")
 
-logger.info("bulding vocabulray")
-[word_vocab, tag_vocab, char_vocab, max_char] = build_vocab([
-    cfg.file_conll_task1_train_data, cfg.file_conll_task1_validation_data, cfg.file_conll_task2_train_data])
+answer = input("continue?")
+if answer == "no":
+    logger.info("bulding vocabulray")
+    [word_vocab, tag_vocab, char_vocab, max_char] = build_vocab([
+        cfg.file_conll_task1_train_data, cfg.file_conll_task1_validation_data, cfg.file_conll_task2_train_data])
 
-logger.info("dumping created vocabulray")
-dump_vocab(word_vocab, cfg.file_word_vocab)
-dump_vocab(tag_vocab, cfg.file_tag_vocab)
-dump_vocab(char_vocab, cfg.file_char_vocab)
+    logger.info("dumping created vocabulray")
+    dump_vocab(word_vocab, cfg.file_word_vocab)
+    dump_vocab(tag_vocab, cfg.file_tag_vocab)
+    dump_vocab(char_vocab, cfg.file_char_vocab)
 
-logger.info("loading created vocabulray again")
-[vocab_id2word, vocab_word2id] = load_vocab(cfg.file_word_vocab)
+    logger.info("loading created vocabulray again")
+    [vocab_id2word, vocab_word2id] = load_vocab(cfg.file_word_vocab)
+    [vocab_id2tag, vocab_tag2id] = load_vocab(cfg.file_tag_vocab)
+    [vocab_id2char, vocab_char2id] = load_vocab(cfg.file_char_vocab)
+
+    logger.info("trimming word embedding")
+    dump_trimmed_word_embeddings(vocab_word2id, cfg.file_full_word_embedding, cfg.file_trimmed_word_embedding,cfg.word_embedding_dimension)
+
 [vocab_id2tag, vocab_tag2id] = load_vocab(cfg.file_tag_vocab)
+[vocab_id2word, vocab_word2id] = load_vocab(cfg.file_word_vocab)
 [vocab_id2char, vocab_char2id] = load_vocab(cfg.file_char_vocab)
-
-logger.info("trimming word embedding")
-dump_trimmed_word_embeddings(vocab_word2id, cfg.file_full_word_embedding, cfg.file_trimmed_word_embedding,cfg.word_embedding_dimension)
 twe = load_trimmed_word_embeddings(cfg.file_trimmed_word_embedding)
 
 logger.info("converting to conll format")
@@ -40,7 +46,6 @@ logger.info("loading data again.")
 [validation_words, validation_tags, validation_chars] = load_sequence_data(cfg.file_seq_task1_validation_data)
 [task1_train_words, task1_train_tags, task1_train_chars] = load_sequence_data(cfg.file_seq_task1_train_data)
 [task2_train_words, task2_train_tags, task2_train_chars] = load_sequence_data(cfg.file_seq_task2_train_data)
-
 
 mod = MTL2CharCNNWordBilstmModel(vocab_size, dim, tag_size, tag_size, cfg, cfg.max_char)
 mod.build_graph()
