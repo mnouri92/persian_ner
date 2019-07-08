3d2099f316b5f20876f4bf42587a9cf49b2ff74d
bokaei <m.hadibokaei@gmail.com>
2019-07-08 12:49:00 +0430

    updates

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # persian_ner
+In order to train the model run the following code. It will get the required files except the word embedding which must be manually placed in 'files/'
+
+```angular2html
+chmod +x train.sh
+./train.sh
+```
+
+In order to evaluate the model with the pretrained model run the following code:
+
+```angular2html
+
+```
+
+
--- train.sh
+++ train.sh
@@ -9,13 +9,13 @@ mkdir files/data/ner_armanperso
 git pull
 
 
-wget https://www.dropbox.com/s/ju6okh5aw24ozyy/train.data?dl=0
+wget https://www.dropbox.com/s/hdqf7j9ftyoccml/train.data?dl=0
 mv train.data?dl=0 files/data/ner_bijankhan/train.data
 
 wget https://www.dropbox.com/s/4fjosovc8e10wro/validation.data?dl=0
 mv validation.data?dl=0 files/data/ner_bijankhan/validation.data
 
-wget https://www.dropbox.com/s/hdqf7j9ftyoccml/train.data?dl=0
+wget https://www.dropbox.com/s/ju6okh5aw24ozyy/train.data?dl=0
 mv train.data?dl=0 files/data/ner_armanperso/train.data
 
 
