b87fb0f66114c8a175972249b076a8b64c7f4e0f
bokaei <m.hadibokaei@gmail.com>
2019-07-08 13:29:54 +0430

    updates

--- train.sh
+++ train.sh
@@ -8,15 +8,22 @@ mkdir files/data/ner_armanperso
 
 git pull
 
+if [ ! -f files/data/ner_bijankhan/train.data ]
+then
+    wget https://www.dropbox.com/s/hdqf7j9ftyoccml/train.data?dl=0
+    mv train.data?dl=0 files/data/ner_bijankhan/train.data
+fi
 
-wget https://www.dropbox.com/s/hdqf7j9ftyoccml/train.data?dl=0
-mv train.data?dl=0 files/data/ner_bijankhan/train.data
+if [ ! -f files/data/ner_bijankhan/validation.data ]
+then
+    wget https://www.dropbox.com/s/4fjosovc8e10wro/validation.data?dl=0
+    mv validation.data?dl=0 files/data/ner_bijankhan/validation.data
+fi
 
-wget https://www.dropbox.com/s/4fjosovc8e10wro/validation.data?dl=0
-mv validation.data?dl=0 files/data/ner_bijankhan/validation.data
-
-wget https://www.dropbox.com/s/ju6okh5aw24ozyy/train.data?dl=0
-mv train.data?dl=0 files/data/ner_armanperso/train.data
-
+if [ ! -f files/data/ner_armanperso/train.data ]
+then
+    wget https://www.dropbox.com/s/ju6okh5aw24ozyy/train.data?dl=0
+    mv train.data?dl=0 files/data/ner_armanperso/train.data
+fi
 
 python script_mtl_train.py
