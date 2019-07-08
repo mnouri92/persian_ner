83e3b38284ac20c6bff309581782c5741031b48d
bokaei <m.hadibokaei@gmail.com>
2019-07-08 12:29:53 +0430

    updates

--- train.sh
+++ train.sh
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-if [ ! -d persian_ner ]; then
-  git clone https://github.com/hadibokaei/persian_ner.git
-  mkdir persian_ner/files
-  mkdir persian_ner/files/data
-  mkdir persian_ner/files/model
-  mkdir persian_ner/files/data/ner_bijankhan
-  mkdir persian_ner/files/data/ner_armanperso
-fi
-cd persian_ner
+mkdir files
+mkdir files/data
+mkdir files/model
+mkdir files/data/ner_bijankhan
+mkdir files/data/ner_armanperso
+
 git pull
 
 
 wget -c https://www.dropbox.com/s/ju6okh5aw24ozyy/train.data?dl=0
-mv train.data?dl=0 persian_ner/files/data/ner_bijankhan/train.data
+mv train.data?dl=0 files/data/ner_bijankhan/train.data
 
 wget -c https://www.dropbox.com/s/4fjosovc8e10wro/validation.data?dl=0
-mv train.data?dl=0 persian_ner/files/data/ner_bijankhan/validation.data
+mv train.data?dl=0 files/data/ner_bijankhan/validation.data
 
 wget -c https://www.dropbox.com/s/hdqf7j9ftyoccml/train.data?dl=0
-mv train.data?dl=0 persian_ner/files/data/ner_armanperso/train.data
+mv train.data?dl=0 files/data/ner_armanperso/train.data
 
-cd persian_ner
 
 python script_mtl_train.py
