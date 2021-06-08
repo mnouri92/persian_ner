a3bd986c7a403f1215ded5b47ce6d923cd4737db
mnouri92 <84831032+mnouri92@users.noreply.github.com>
2021-06-08 18:47:24 +0430

    Update evaluate_mtl.sh

--- evaluate_mtl.sh
+++ evaluate_mtl.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Illegal number of parameters. The firts parameter is the path of the input directory. The second parameter is the path of the output directory..."
+if [ "$#" -ne 3 ]; then
+    echo "Illegal number of parameters. The first parameter is the type of model, second parameters is the path of the input directory, and The third parameter is the path of the output directory..."
     exit 1
 fi
 
 pip install -r requirement.txt
 
-mkdir $2
+mkdir $3
 
-python script_evaluate.py "mtl" "files/mtl/ner_bijankhan/" $1 $2
+python script_evaluate.py $1 "files/mtl/ner_bijankhan/" $2 $3
 
 
