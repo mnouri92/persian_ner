90da1516cd92c41dde55df796643714753f8c4c3
mnouri92 <84831032+mnouri92@users.noreply.github.com>
2021-06-08 18:45:24 +0430

    Update train_mtl.sh

--- train_mtl.sh
+++ train_mtl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-    echo "Illegal number of parameters. The firts parameter must be the path of the word embedding file."
+    echo "Illegal number of parameters. The firts parameter must be the choosed model to train ( mtl2, mtl3 or mtl4 )."
     exit 1
 fi
 
@@ -12,8 +12,19 @@ mkdir files/mtl
 mkdir files/mtl/ner_bijankhan
 mkdir files/mtl/ner_bijankhan/data
 mkdir files/mtl/ner_bijankhan/model
+mkdir files/mtl/ner_bijankhan/saved_model
 mkdir files/mtl/ner_armanperso
 mkdir files/mtl/ner_armanperso/data
+mkdir files/mtl/ner_armanperso/model
+mkdir files/mtl/ner_armanperso/saved_model
+mkdir files/mtl/gen
+mkdir files/mtl/gen/data
+mkdir files/mtl/gen/model
+mkdir files/mtl/gen/saved_model
+mkdir files/mtl/pos
+mkdir files/mtl/pos/data
+mkdir files/mtl/pos/model
+mkdir files/mtl/pos/saved_model
 
 if [ ! -f files/mtl/ner_bijankhan/data/train.data ]
 then
@@ -27,10 +38,54 @@ then
     mv train.data?dl=0 files/mtl/ner_armanperso/data/train.data
 fi
 
-if [ ! -f files/mtl/ner_bijankhan/we.vec ]
+if [ ! -f files/mtl/gen/data/train.data ]
 then
-    cp "$1" "files/mtl/ner_bijankhan/we.vec"
+    wget https://www.gendata.com
+    mv train.data files/mtl/gen/data/train.data
 fi
 
-python script_train.py mtl2 files/mtl/ner_bijankhan/model files/mtl/ner_bijankhan/we.vec files/mtl/ner_bijankhan files/mtl/ner_armanperso
+if [ ! -f files/mtl/pos/data/train.data ]
+then
+    wget https://posdata.com
+    mv train.data files/mtl/pos/data/train.data
+fi
 
+if [ ! -f files/mtl/ner_bijankhan/we.vec ]
+then
+    wget https://wordemb.com/we.vec
+    mv we.vec files/mtl/ner_bijankhan/we.vec
+fi
+
+
+if [ "$1" == "mtl2" ]
+then
+    if [ "$#" -ne 3 ]
+    then
+        echo "please use the shell correctly : ( ./train.sh mtl2 <main_task_path> <auxiliary_task_path> )"
+        exit 1
+    fi
+    python script_train.py $1 $2/model files/mtl/ner_bijankhan/we.vec $2 $3
+fi
+
+
+if [ "$1" == "mtl3" ]
+then
+    if [ "$#" -ne 4 ]
+    then
+        echo "please use the shell correctly : ( ./train.sh mtl3 <main_task_path> <first_aux_task_path> <second_aux_task_path> )"
+        exit 1
+    fi
+    python script_train.py $1 $2/model files/mtl/ner_bijankhan/we.vec $2 $3 $4
+fi
+
+
+
+if [ "$1" == "mtl4" ]
+then
+    if [ "$#" -ne 5 ]
+    then
+        echo "please use the shell correctly : ( ./train.sh mtl4 <main_task_path> <first_aux_task_path> <second_aus_task_path> <third_aux_task_path> )"
+        exit 1
+    fi
+    python script_train.py $1 $2/model files/mtl/ner_bijankhan/we.vec $2 $3 $4 $5
+fi
